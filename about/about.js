b9603d418ddfed6d18db6b9c49ac0d247ff354a1
KoishiChan <qwq@koishi.top>
2019-08-04 00:25:47 +0800

    gone

--- about/about.js
+++
@@ -1,7 +0,0 @@
-// $(function(){
-// 	$(".fold-button").click(function(){
-// 		$(".fold-box").slideToggle("slow");
-// 		$(".open").toggle();
-// 		$(".close").toggle();
-// 	});
-// });
--- ext/webgl.bee/shaders/gl2/fftend.vert
+++
@@ -1,8 +0,0 @@
-#version 300 es
-
-in vec2 Position;
-
-void main()
-{
-	gl_Position = vec4(Position, 0, 1);
-}
